f319a624150513b8698800f8f0806c7109f1310e
Paulo Henrique Serafim <phls321@hotmail.com>
2023-09-05 22:52:51 -0300

    Login

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import requests
 from bs4 import BeautifulSoup
 
@@ -51,7 +54,23 @@ def _scrapper(links, headers):
     return carros
 
 def _post(carros):
-    print("Olá")
+    driver = webdriver.Firefox()
+    driver.get("http://weka.inf.ufes.br/IFESTP/login.php")
+    time.sleep(2)
+    #Realizar login
+    login = driver.find_element(By.ID, "username")
+    senha = driver.find_element(By.ID, "password")
+    login.send_keys("Peaga")
+    senha.send_keys("guilherme_play")
+    btnLogin = driver.find_element(By.NAME, "submit")
+    btnLogin.click()
+    time.sleep(2)
+    #Acessa página de cadastro
+    btnNovo = driver.find_element(By.CLASS_NAME, "btn")
+    btnNovo.click()
+    time.sleep(2)
+    #Adiciona veiculo
+    txtMarca = driver.find_element(By.ID, "marca")
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
